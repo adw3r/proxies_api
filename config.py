91b9722720d5a2e65093af1cc890f03e9749f890
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-02-10 16:30:16 +0200

    fix

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
+import configparser
 import os
 import pathlib
 
-from dotenv import load_dotenv
-
-load_dotenv()
 PACKAGE_FOLDER = pathlib.Path(__file__).parent
 PROXIES_FOLDER = pathlib.Path(PACKAGE_FOLDER, 'proxies')
 if not PROXIES_FOLDER.exists():
     os.mkdir(PROXIES_FOLDER)
 
-HOST = os.environ.get('HOST', '0.0.0.0')
-PORT = os.environ.get('PORT', int('8181'))
+config = configparser.ConfigParser()
+config.read(pathlib.Path(PACKAGE_FOLDER, 'config.ini'))
+
+HOST = config['general'].get('HOST', 'localhost')
+PORT = config['general'].getint('PORT', 8182)
