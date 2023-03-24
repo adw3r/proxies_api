ff19eacf80d3134a16f7511aae0ff65cf8345502
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-03-24 11:40:11 +0200

    fix

--- check_proxies.py
+++ check_proxies.py
@@ -4,15 +4,15 @@ import aiohttp
 import loguru
 import requests
 
+PROXIES = 'http://localhost:8182/proxies'
 URL = 'http://ip-api.com/json/?fields=8217'
-
+SEMAPHORE = asyncio.Semaphore(5000)
 logger = loguru.logger
-semaphore = asyncio.Semaphore(5000)
 
 
 async def check_proxy(proxy: str) -> dict | None:
     try:
-        async with semaphore:
+        async with SEMAPHORE:
             async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(30)) as session:
                 async with session.get(URL, proxy=proxy) as response:
                     json_response = await response.json()
@@ -27,7 +27,7 @@ async def check_proxy(proxy: str) -> dict | None:
 
 async def check_pool(pool):
     logger.info(pool)
-    proxies = requests.get(f'http://localhost:8182/proxies/{pool}',
+    proxies = requests.get(f'{PROXIES}/{pool}',
                            params={'method': 'pool'}).text.strip().splitlines()
     cors = [asyncio.create_task(check_proxy(proxy)) for proxy in proxies]
     for res in asyncio.as_completed(cors):
@@ -37,7 +37,7 @@ async def check_pool(pool):
 
 
 async def check_pools():
-    pools = requests.get('http://localhost:8182/proxies').json().keys()
+    pools = requests.get(PROXIES).json().keys()
     logger.info(pools)
     for pool in pools:
         await check_pool(pool)
