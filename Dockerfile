080be45c19f797adb7948792809a36a25d3b1c2d
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-04-02 22:00:11 +0300

    ...

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM python:3.10
 
-WORKDIR /myapp
+WORKDIR /app
+
+
+COPY requirements.txt .
 
 RUN pip install -r requirements.txt
-RUN pip install -e .
-
 
 COPY . .
 CMD ["sh", "-c", "uvicorn main:app --host=$HOST --port=$PORT"]
--- tests/test_enpoints.py
+++ tests/test_enpoints.py
@@ -4,31 +4,7 @@ import requests
 
 
 class TestEndpoints(TestCase):
-
-    def test_post_pool(self):
-        file_bytes = '''
-        http://localhost:8000        
-        '''.encode()
-        url = 'http://localhost:8282/proxies/testpool'
-        values = {
-            'file': ('test.txt', file_bytes),
-        }
-        resp = requests.post(url, files=values)
-        print(resp.text)
-
-    def test_patch_proxies(self):
-        url = 'http://localhost:8282/proxies'
-        values = {
-            'parsed': 'test_parsed.txt',
-        }
-        resp_value = requests.patch(url, json=values)
-        print(resp_value.text)
-
-    def test_post_proxies(self):
-        url = 'http://localhost:8282/proxies'
-        post_values = {
-            "parsed": "parsed.txt",
-            "west": "west.txt",
-        }
-        value = requests.post(url, json=post_values)
-        print(value.text)
+    def test_get_pool(self):
+        url = 'http://localhost:8182/proxies/parsed?method=pool'
+        resp_value = requests.get(url).text
+        self.assertTrue('http' in resp_value)
